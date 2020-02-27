efe8df0dea298d1eee19d4740e22bdaccc76e47d
muriloignaccio <murilohsignacio@gmail.com>
2020-02-26 21:44:39 -0300

    finalized node petshop challenge

--- aula_5/petshop-node/index.js
+++ aula_5/petshop-node/index.js
@@ -6,26 +6,47 @@ const server = http.createServer((req, res) => {
     let urlCompleta = url.parse(req.url, true);
     let queryString = urlCompleta.query;
     let caminho = urlCompleta.pathname;
-    switch(caminho){
+    switch (caminho) {
         case "/pets":
             let conteudo = petshop.listarPets();
             conteudo.length > 0 ? res.write(conteudo) : res.write("Nenhum pet cadastrado :(")
             break;
         case "/pets/add":
             let novoPet = queryString;
-            if(petshop.adicionarPet(novoPet)){
+            if (petshop.adicionarPet(novoPet)) {
                 res.write(`${novoPet.nome} foi adicionado com sucesso!`);
-            }else{
-                res.write("deu errado");
+            } else {
+                res.write("Ops, insira um argumento valido!");
             }
             break;
         case "/pets/buscar":
-            let nomePet = queryString.nome;
-            let petEncontrado = petshop.buscar(nomePet);
-            petEncontrado.length > 0 ? res.write(`Foi encontrado ${petEncontrado.length} pets`) : res.write("Não foi encontrado nenhum pet");
+            let { nome } = queryString;
+            let petEncontrado = petshop.buscarPetPorNome(nome);
+            if (petEncontrado.length == 1) {
+                // res.write(`Foi encontrado ${petEncontrado.length} pets`);
+                res.write(JSON.stringify(petEncontrado.pop()));
+            } else if (petEncontrado.length > 1){
+                res.write(JSON.stringify(petEncontrado));
+                res.write(`\n${petEncontrado.length} pets foram encontrados!`);
+            } 
+            else {
+                res.write("Não foi encontrado nenhum pet");
+            }
+            break;
+        case "/campanha":
+            res.write("Campanha de vacina 2020!");
+            res.write(petshop.contarVacinados());
+            res.write("Vacinando...\n");
+            res.write(petshop.campanhaVacina());
+            break;
+        case "/atendimento":
+            let { nome: nomePet, servicos } = queryString;
+            servicos = servicos.split(",");
+            res.write(`Bem vindo, ${nomePet}\n`);
+            res.write(petshop.atenderPet(nomePet, res, ...servicos));
             break;
         default:
             res.write("to perdido me ajuda pfvr");
     }
     res.end();
-}).listen(3000, "localhost", () => console.log("Servidor rodando"));
+}).listen(3000, () => console.log("Servidor rodando"));
--- aula_5/petshop-node/petshop.js
+++ aula_5/petshop-node/petshop.js
@@ -1,15 +1,170 @@
-let pets = [{nome: "Nego"}, {nome: "Fezes"}];
-let listarPets = () => {
-    let conteudo = "";
-    pets.forEach(pet => {
-        conteudo += `
-        ----------
-        Nome: ${pet.nome}
-        ----------
-        `;
-    })
-    return conteudo;
-}
-const adicionarPet = novoPet => pets.push(novoPet);
-const buscar = nomePet => pets.filter(pet => pet.nome == nomePet);  
-module.exports = {listarPets, adicionarPet, buscar};
+const pets = [{
+	nome: "Batman",
+	tipo: "cão",
+	raca: "labrador",
+	idade: 5,
+	genero: "M",
+	vacinado: false,
+	servicos: ["banho", "tosa"]
+},
+{
+	nome: "Costelinha",
+	tipo: "cão",
+	raca: "vira-lata",
+	idade: 10,
+	genero: "M",
+	vacinado: true,
+	servicos: ["banho"]
+},
+{
+	nome: "Scooby Doo",
+	tipo: "cão",
+	raca: "Dogue Alemão",
+	idade: 51,
+	genero: "M",
+	vacinado: false,
+	servicos: ["banho", "tosa"]
+},
+{
+	nome: "Tom",
+	tipo: "gato",
+	raca: "poodle",
+	idade: 5,
+	genero: "M",
+	vacinado: false,
+	servicos: ["corte de unha"]
+},
+{
+	nome: "Ada",
+	tipo: "iguana",
+	raca: "albina",
+	idade: 5,
+	genero: "F",
+	vacinado: true,
+	servicos: ["banho"]
+}];
+
+const listarPets = () => {
+	let conteudo = "";
+	for (let pet of pets) {
+    conteudo += `
+    ------------------------
+    Nome: ${pet.nome}
+    Tipo: ${pet.tipo}
+    Raça: ${pet.raca}
+    Idade: ${pet.idade} anos
+    Genero: ${pet.genero == "F" ? "Fêmea" : "Macho"}
+    Vacinado: ${pet.vacinado ? "Sim" : "Não"}
+    Serviços: ${pet.servicos}`;
+  }
+  return conteudo;
+};
+
+const buscarPetPorNome = nomePet => pets.filter(pet => pet.nome === nomePet);
+
+const validarDados = novoPet => {
+  return (
+    novoPet.nome &&
+    novoPet.idade &&
+    novoPet.tipo &&
+    novoPet.raca &&
+    novoPet.genero
+  );
+};
+
+const adicionarPet = novoPet => {
+  if (typeof novoPet == "object" && validarDados(novoPet)) {
+
+    novoPet.nome = String(novoPet.nome);
+    novoPet.idade = parseInt(novoPet.idade);
+
+    if (!novoPet.servicos) {
+      novoPet.servicos = [];
+    }
+
+		pets.push(novoPet);
+		return `${novoPet.nome} adicionadx com sucesso!`
+  } else {
+    return false;
+  }
+};
+
+const vacinarPet = nomePet => {
+	const pet = buscarPetPorNome(nomePet);
+	if (!pet.vacinado) {
+		pet.vacinado = true;
+		return `${pet.nome} foi vacinado com sucesso!`;
+	} else {
+		return `Ops, ${pet.nome} já está vacinado!`;
+	}
+};
+
+const contarVacinados = () => {
+	const vacinados = pets.filter(pet => pet.vacinado).length;
+	const naoVacinados = pets.filter(pet => !pet.vacinado).length;
+
+	return `
+	------------------------
+	Temos ${vacinados} pets vacinados.
+	Temos ${naoVacinados} pets NÃO vacinados.
+	------------------------
+	`;
+};
+
+const campanhaVacina = () => {
+	let petVacinadosCampanha = 0;
+	for (let pet of pets) {
+		vacinarPet(pet);
+		petVacinadosCampanha++;
+	}
+	return `${petVacinadosCampanha} pets foram vacinados nessa campanha!`;
+};
+
+const darBanhoPet = nomePet => {
+	const pet = buscarPetPorNome(nomePet).pop();
+  pet.servicos.push("banho");
+  return `${nomePet} está de banho tomado!`;
+};
+
+const tosarPet = nomePet => {
+	const pet = buscarPetPorNome(nomePet).pop();
+  pet.servicos.push("tosa");
+  return `${nomePet} está com cabelinho na régua :)`;
+};
+
+const apararUnhasPet = nomePet => {
+	const pet = buscarPetPorNome(nomePet).pop();
+  pet.servicos.push("corte de unhas");
+	return `${nomePet} está de unhas aparadas!`;
+};
+
+
+const atenderPet = (nomePet, res, ...servicosSolicitados) => {
+	
+	const servicosCadastrados = [darBanhoPet, tosarPet, apararUnhasPet];
+
+	let conteudo = "";
+
+	servicosSolicitados.forEach(cadaServico => {
+		servicosCadastrados.forEach(servico => {
+			if (cadaServico.trim() == servico.name) {
+				conteudo += servico(nomePet, res) + "\n";
+			}
+		});
+	})
+	
+  const pagar = () => {
+    return "Pagamento realizado com sucesso!";
+  };
+
+  return `${conteudo}${pagar()}\nVolte sempre, ${nomePet}`;
+};
+  
+module.exports = { 
+	listarPets, 
+	adicionarPet, 
+	buscarPetPorNome, 
+	campanhaVacina, 
+	contarVacinados,
+	atenderPet
+};
